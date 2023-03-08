59218a1518589e9f30b8da6232d475bd4e892bc2
Vũ Phạm Tiến Dũng <43530592+vuphamtiendung@users.noreply.github.com>
2023-03-08 14:26:41 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RazorViewEngine.Models;
+
+namespace RazorViewEngine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public List<ProductModel> products = new List<ProductModel>()
+        {
+            new ProductModel()
+            {
+                Id = 1,
+                Name = "Product 1",
+                Available = true,
+                Price = 550000,
+                PromotionPrice = 50000
+            },
+
+            new ProductModel()
+            {
+                Id = 2,
+                Name = "Product 2",
+                Available = false,
+                Price = 730000,
+                PromotionPrice = 20000
+            },
+
+            new ProductModel()
+            {
+                Id = 3,
+                Name = "Product 3",
+                Available = true,
+                Price = 850000,
+                PromotionPrice = 30000
+            },
+
+            new ProductModel()
+            {
+                Id = 4,
+                Name = "Product 4",
+                Available = true,
+                Price = 220000,
+                PromotionPrice = 10000
+            },
+
+            new ProductModel()
+            {
+                Id = 5,
+                Name = "Product 5",
+                Available = false,
+                Price = 730000,
+                PromotionPrice = 20000
+            },
+
+            new ProductModel()
+            {
+                Id = 6,
+                Name = "Product 6",
+                Available = true,
+                Price = 990000,
+                PromotionPrice = 10000
+            },
+        };
+
+        public IActionResult Product()
+        { 
+            return View(products);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorViewEngine.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public bool Available { get; set; } 
+        public double Price { get; set; }   
+        public double PromotionPrice { get; set; }
+    }
+}
